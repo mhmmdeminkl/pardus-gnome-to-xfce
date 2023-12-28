da799a61f41997b5ba9a00a1ac1e762f6eaee5be
Muhammed Emin Kola <152064891+mhmmdeminkl@users.noreply.github.com>
2023-12-28 19:20:08 +0300

    Update pgtx.sh
    cd komutu düzeltme denemesi yapılacak

--- pgtx.sh
+++ pgtx.sh
@@ -55,7 +55,7 @@ confirm() {
 }
 
 # Pardus Karşılayıcı için autostart işlemi
-cd pardus-gnome-to-xfce
+cd Masaüstü/pardus-gnome-to-xfce
 cp parduskarşılayıcı.desktop /.config/autostart
  
 # Verilen girdiye göre işlemin devamı
