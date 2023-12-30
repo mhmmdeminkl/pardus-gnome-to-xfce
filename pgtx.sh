d64be3cbde0d4e5d6ef497e64fed4f5e2ed7885d
Muhammed Emin Kola <152064891+mhmmdeminkl@users.noreply.github.com>
2023-12-30 20:57:35 +0300

    Update pgtx.sh
    Paket isimleri ayrı yazıldı, bazı düzenlemeler yapıldı.

--- pgtx.sh
+++ pgtx.sh
@@ -18,7 +18,7 @@ confirm() {
  
 # Verilen girdiye göre işlemin devamı veya iptali
 if confirm; then
-    echo "İşleme devam ediliyor lütfen bekleyiniz..."
+    echo "İşleme devam ediliyor. Lütfen root yetkisi için şifrenizi girebilir misiniz?"
 else
     echo "İşlem iptal edildi. Çıkış yapılıyor. Tekrar görüşmek üzere..."
     exit
@@ -27,19 +27,31 @@ fi
 # Güncelleme, XFCE yükleme işlemi ve GNOME arayüzüne dair tüm paketlerin silinmesi
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install pardus-xfce-*
+sudo apt-get -y install pardus-xfce-desktop
+sudo apt-get -y install pardus-xfce-greeter
+sudo apt-get -y install pardus-xfce-gtk-theme
+sudo apt-get -y install pardus-xfce-icon-theme
+sudo apt-get -y install pardus-xfce-live-settings
+sudo apt-get -y install pardus-xfce-settings
+sudo apt-get -y install pardus-xfce-tweaks
 sudo dpkg-reconfigure lightdm
 sudo apt-get -y purge gdm3
 sudo apt-get -y purge gdm
-sudo apt-get -y purge pardus-gnome-*
+sudo apt-get -y install pardus-gnome-desktop
+sudo apt-get -y install pardus-gnome-greeter
+sudo apt-get -y install pardus-gnome-settings
+sudo apt-get -y install pardus-gnome-shortcuts
 sudo apt-get -y purge gnome-desktop3-data
+# XFCE arayüzünün GNOME arayüzü ile ilgili paketleri kullanma ihtimali bulunduğu için tekrar kullanıldı.
+sudo apt-get -y pardus-xfce-*
 sudo apt-get -y autoremove
 sudo apt-get -y clean
 sudo apt-get -y autoclean
 
+# Pardus Karşılayıcı için autostart işlemi
+`cp /usr/share/applications/tr.org.pardus.xfce-greeter.desktop ~/.config/autostart/`
+
 echo "İşlem tamamlandı. Başka scriptlerde görüşmek üzere..."
-
-
 
 echo -e  "\033[31mDeğişikliklerin uygulanabilmesi için bilgisayarınız yeniden başlatılacaktır. Bu soruya onay vermelisiniz!\033[0m"
 
@@ -54,8 +66,6 @@ confirm() {
     done
 }
 
-# Pardus Karşılayıcı için autostart işlemi
-`cp /usr/share/applications/tr.org.pardus.xfce-greeter.desktop ~/.config/autostart/`
  
 # Verilen girdiye göre işlemin devamı
 if confirm; then
